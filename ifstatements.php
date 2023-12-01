6490ff936b800cc585bd25f3694771d66640400f
tony555t <tonytullah764@gmail.com>
2023-12-01 15:55:59 +0300

    if statements

---
+++ ifstatements.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+if(4>10)
+{
+
+
+    echo"three is less than 10";
+}elseif
+{
+    echo "it is note";
+}
+elseif(4>6){
+    echo "hhhjfjfjfjfjjf";
+
+}
+else{
+    echo "giv new number";
+}
+
+
+?>
+    
+</body>
+</html>
