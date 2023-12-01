2d0442ec46ea7b57597b823930d66bbe34d407d4
tony555t <tonytullah764@gmail.com>
2023-12-01 12:13:43 +0300

    initial commit

--- array.php
+++ array.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+$numberList=array(8940,6783,9063);
+//echo $numberlist[0];
+print_r ($numberlist);
+echo $numberList[5];
+
+?>
 </body>
 </html>
