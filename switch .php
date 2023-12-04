8da09d2e9cc0d10ca2ae6bd0f1d670add400a64f
tony555t <tonytullah764@gmail.com>
2023-12-04 10:22:17 +0300

    switch statements

---
+++ switch .php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $number =4;
+    if($number <10){
+        echo 'this is good';
+    }
+    $number =4;
+    if($number <20){
+        echo 'this is better';
+    }
+    $number =4;
+    if($number <30){
+        echo 'this is best';
+    }
+
+
+    ?>
+
+
+</body>
+</html>
