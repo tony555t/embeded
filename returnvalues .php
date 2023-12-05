f2cd1e5e93b3c504a60b37693a5aa6e4097eb4c3
tony555t <tonytullah764@gmail.com>
2023-12-05 11:04:19 +0300

    results

--- returnvalues .php
+++ returnvalues .php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+function addNumber($number1,$number2){
+    $sum = $number1 + $number2;
+    return $sum;
+
+}
+$result=addNumber(89,90);
+$result =addNumber(80,67);
+$result=addNumber(45,90);
+?>
 </body>
 </html>
