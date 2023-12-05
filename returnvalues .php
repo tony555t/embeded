4642e045cb43900b0659937e4ffb0d75ea74d15b
tony555t <tonytullah764@gmail.com>
2023-12-05 11:39:29 +0300

    calculate

--- returnvalues .php
+++ returnvalues .php
@@ -15,6 +15,32 @@ function addNumber($number1,$number2){
 $result=addNumber(89,90);
 $result =addNumber(80,67);
 $result=addNumber(45,90);
+
+ function getGreeting($name){
+    $greeting = "hello".$name."welcome back.";
+    return $greeting;
+
+ }
+ $userName="john";
+ $welcomeMessage =getGreeting($userName);
+ echo $welcomeMessage;
+
+
+ function calculateGrade($marks){
+    if ($marks >= 90) {
+        return"a";
+    }elseif($marks >=80){
+        return "b";
+    }elseif($marks>=70){
+        return "c";
+    }elseif($marks>=60){
+        return "d";
+    }else{
+        return"f";
+    }
+}
+$studentMarks =85;
+$grade = calculateGrade($studentMarks)
 ?>
 </body>
 </html>
